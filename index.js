90afb470f83e0edd8ec7d96985856b92d18df9f9
Dhamotharan281 <97104022+Dhamotharan281@users.noreply.github.com>
2024-09-06 05:00:25 +0530

    Add files via upload
    welcome all this project is fully Responsive Eommerce site

---
+++ index.js
@@ -0,0 +1,16 @@
+var sidenav=document.querySelector(".side-navbar")//to select class
+
+
+function showNavbar(){
+    //sidenav.style.display="block";
+    sidenav.style.left="0"
+
+}
+
+
+function closeNavbar(){
+   // sidenav.style.display="none";
+   sidenav.style.left="-60%"
+
+
+}
